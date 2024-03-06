294827c7bf82813e7c9ff157ad4bef91a1d9b2cb
locpython <138984694+locpython@users.noreply.github.com>
2024-03-06 21:57:39 +0700

    Add files via upload

---
+++ khoi.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pickle
+st.title("Revenue Prediction")
+st.balloons()
+st.snow()
+pred = st.slider('Input Temperature', 0, 45, 25)
+df = pd.read_csv("C:\\Users\\lusan\\OneDrive\\Desktop\\IceCreamData.csv")
+x = df['Temperature'].values.reshape(-1, 1)
+y = df['Revenue'].values.reshape(-1, 1)
+model = LinearRegression()
+model.fit(x, y)
+y_pred = model.predict([[pred]])
+k = y_pred[0][0]
+st.write(f"Revenue Prediction: {round(k,1)}")
+
